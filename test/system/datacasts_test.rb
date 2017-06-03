e666aec20851489ad618ebd55cf79f64b4882c5b
Ritvvij Parrikh <ritvvij.parrikh@pykih.com>
2017-06-03 22:58:45 +0530

    s

--- test/system/datacasts_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class DatacastsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit datacasts_url
-  #
-  #   assert_selector "h1", text: "Datacast"
-  # end
-end
