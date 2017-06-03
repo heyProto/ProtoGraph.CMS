26b713e15191cbf4f630ff245a3dd6acc38275a7
Ritvvij Parrikh <ritvvij.parrikh@pykih.com>
2017-06-03 19:07:50 +0530

    s

--- app/controllers/template_data_controller.rb
+++ app/controllers/template_data_controller.rb
@@ -1,48 +1,35 @@
 class TemplateDataController < ApplicationController
+
+  before_action :authenticate_user!
   before_action :set_template_datum, only: [:show, :edit, :update, :destroy]
 
-  # GET /template_data
-  # GET /template_data.json
   def index
     @template_data = TemplateDatum.all
   end
 
-  # GET /template_data/1
-  # GET /template_data/1.json
   def show
   end
 
-  # GET /template_data/new
   def new
     @template_datum = TemplateDatum.new
   end
 
-  # GET /template_data/1/edit
   def edit
   end
 
-  # POST /template_data
-  # POST /template_data.json
   def create
     @template_datum = TemplateDatum.new(template_datum_params)
-
-    respond_to do |format|
-      if @template_datum.save
-        format.html { redirect_to @template_datum, notice: 'Template datum was successfully created.' }
-        format.json { render :show, status: :created, location: @template_datum }
-      else
-        format.html { render :new }
-        format.json { render json: @template_datum.errors, status: :unprocessable_entity }
-      end
+    if @template_datum.save
+      redirect_to @template_datum, notice: t("cs")
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /template_data/1
-  # PATCH/PUT /template_data/1.json
   def update
     respond_to do |format|
       if @template_datum.update(template_datum_params)
-        format.html { redirect_to @template_datum, notice: 'Template datum was successfully updated.' }
+        format.html { redirect_to @template_datum, notice: t("us") }
         format.json { render :show, status: :ok, location: @template_datum }
       else
         format.html { render :edit }
@@ -51,23 +38,20 @@ class TemplateDataController < ApplicationController
     end
   end
 
-  # DELETE /template_data/1
-  # DELETE /template_data/1.json
   def destroy
     @template_datum.destroy
     respond_to do |format|
-      format.html { redirect_to template_data_url, notice: 'Template datum was successfully destroyed.' }
+      format.html { redirect_to template_data_url, notice: t("ds")
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_template_datum
-      @template_datum = TemplateDatum.find(params[:id])
+      @template_datum = TemplateDatum.friendly.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def template_datum_params
       params.require(:template_datum).permit(:account_id, :name, :description, :slug, :version, :is_current_version, :status, :api_key, :publish_count, :created_by, :updated_by)
     end
--- app/controllers/template_streams_controller.rb
+++ app/controllers/template_streams_controller.rb
@@ -1,48 +1,35 @@
 class TemplateStreamsController < ApplicationController
+
+  before_action :authenticate_user!
   before_action :set_template_stream, only: [:show, :edit, :update, :destroy]
 
-  # GET /template_streams
-  # GET /template_streams.json
   def index
     @template_streams = TemplateStream.all
   end
 
-  # GET /template_streams/1
-  # GET /template_streams/1.json
   def show
   end
 
-  # GET /template_streams/new
   def new
     @template_stream = TemplateStream.new
   end
 
-  # GET /template_streams/1/edit
   def edit
   end
 
-  # POST /template_streams
-  # POST /template_streams.json
   def create
     @template_stream = TemplateStream.new(template_stream_params)
-
-    respond_to do |format|
-      if @template_stream.save
-        format.html { redirect_to @template_stream, notice: 'Template stream was successfully created.' }
-        format.json { render :show, status: :created, location: @template_stream }
-      else
-        format.html { render :new }
-        format.json { render json: @template_stream.errors, status: :unprocessable_entity }
-      end
+    if @template_stream.save
+      redirect_to @template_stream, notice: t("cs")
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /template_streams/1
-  # PATCH/PUT /template_streams/1.json
   def update
     respond_to do |format|
       if @template_stream.update(template_stream_params)
-        format.html { redirect_to @template_stream, notice: 'Template stream was successfully updated.' }
+        format.html { redirect_to @template_stream, notice: t("us") }
         format.json { render :show, status: :ok, location: @template_stream }
       else
         format.html { render :edit }
@@ -51,23 +38,17 @@ class TemplateStreamsController < ApplicationController
     end
   end
 
-  # DELETE /template_streams/1
-  # DELETE /template_streams/1.json
   def destroy
     @template_stream.destroy
-    respond_to do |format|
-      format.html { redirect_to template_streams_url, notice: 'Template stream was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to template_streams_url, notice: t("ds")
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_template_stream
-      @template_stream = TemplateStream.find(params[:id])
+      @template_stream = TemplateStream.friendly.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def template_stream_params
       params.require(:template_stream).permit(:account_id, :name, :description, :slug, :version, :is_current_version, :status, :publish_count, :created_by, :updated_by)
     end
