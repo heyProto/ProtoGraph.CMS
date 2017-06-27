7f2aa86439b50be1d2d30acfaa1ba50783ac216b
Amit Badheka <ab@pykih.com>
2017-06-27 18:54:36 +0530

    Integrates edit file functionality

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,1 +1,5 @@
-//= require ./form/form-2.js
+//= require ./form/react.min.js
+//= require ./form/react-dom.min.js
+//= require ./form/react-jsonschema-form.js
+//= require ./form/axios.min.js
+//= require ./form/form.js
