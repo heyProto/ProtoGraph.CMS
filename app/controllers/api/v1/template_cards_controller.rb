2c5120441ea2c20ef2a56d0d8da01cebfaa180a1
Ritvvij Parrikh <ritvvij.parrikh@pykih.com>
2017-06-27 17:04:35 +0530

    s

--- app/controllers/api/v1/template_cards_controller.rb
+++ app/controllers/api/v1/template_cards_controller.rb
@@ -2,13 +2,13 @@ class Api::V1::TemplateCardsController < ApiController
 
     def index
         @template_cards = @account.template_cards.where(is_current_version: true)
-        render json: {template_cards: @template_cards.as_json(only: [:account_id, :id, :slug, :global_slug,:name, :elevator_pitch, :template_datum_id, :git_repo_name], methods: [:account_slug, :icon_url])}
+        render json: {template_cards: @template_cards.as_json(only: [:account_id, :id, :slug, :global_slug,:name, :template_datum_id, :git_repo_name], methods: [:account_slug, :icon_url])}
     end
 
     def show
         @template_card = @account.template_cards.where(id: params[:id]).first
         if @template_card.present?
-            render json: {template_card: @template_card.as_json(only: [:account_id, :id, :slug, :global_slug,:name, :elevator_pitch,:template_datum_id, :git_repo_name], methods: [:account_slug, :files, :versions])}
+            render json: {template_card: @template_card.as_json(only: [:account_id, :id, :slug, :global_slug,:name, :elevator_pitch, :description, :template_datum_id, :git_repo_name], methods: [:account_slug, :files, :versions])}
         else
             return_render_card_not_found
         end
