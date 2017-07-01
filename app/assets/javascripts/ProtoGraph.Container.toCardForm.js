62ac3c7da4645985747df3833a5be69c80ecc305
Amit Badheka <ab@pykih.com>
2017-07-01 18:33:19 +0530

    ToShare save working

--- app/assets/javascripts/ProtoGraph.Container.toCardForm.js
+++ app/assets/javascripts/ProtoGraph.Container.toCardForm.js
@@ -2,4 +2,4 @@
 //= require ./form/react-dom.min.js
 //= require ./form/react-jsonschema-form.js
 //= require ./form/axios.min.js
-//= require ./form/form.js
+//= require ./form/ProtoGraph.Container.toCardForm.min.js
