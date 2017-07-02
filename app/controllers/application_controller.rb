6989e8a466a43a5d15e5b146662d1c1852d6f28e
Ritvvij Parrikh <ritvvij.parrikh@pykih.com>
2017-07-02 20:05:12 +0530

    s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, unless: :json_request?
   protect_from_forgery with: :null_session, if: :json_request?
-  before_action :sudo, unless: :json_request?
+  before_action :sudo
 
   def json_request?
     request.format.json?
