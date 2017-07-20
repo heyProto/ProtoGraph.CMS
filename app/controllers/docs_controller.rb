3203711b38540a633860af4e536bcccaafbe31e3
Ritvvij Parrikh <ritvvij.parrikh@pykih.com>
2017-07-20 10:52:00 +0530

    docs

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,27 @@
+class DocsController < ApplicationController
+
+  layout "docs"
+
+  def index
+
+  end
+
+  def cards
+  end
+
+  def cards_build_your_own
+  end
+
+  def cards_styleguide_icons
+  end
+
+  def cards_design_recommendation
+  end
+
+  def cards_publishing
+  end
+
+  def streams
+  end
+
+end
