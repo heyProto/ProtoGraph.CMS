943ead3fd6da2c60f024f9767382ec1a70b7d568
Amit Badheka <ab@pykih.com>
2017-08-17 12:14:45 +0530

    Changes in form

--- app/controllers/api/v1/datacasts_controller.rb
+++ app/controllers/api/v1/datacasts_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::DatacastsController < ApiController
     def create
         payload = {}
         payload["payload"] = datacast_params.to_json
-        payload["source"]  = "form"
+        payload["source"]  = params[:source] || "form"
         view_cast = @account.view_casts.new(view_cast_params)
         view_cast.created_by = @user.id
         view_cast.updated_by = @user.id
@@ -47,7 +47,7 @@ class Api::V1::DatacastsController < ApiController
     end
 
     def view_cast_params
-        params.require(:view_cast).permit(:template_datum_id, :name, :template_card_id, :optionalConfigJSON, :account_id, :updated_by, :seo_blockquote)
+        params.require(:view_cast).permit(:datacast_identifier, :template_datum_id, :name, :template_card_id, :optionalConfigJSON, :account_id, :updated_by, :seo_blockquote)
     end
 
 end
