5a73e04ce8e9d302920c3121e93eef43f2b4c264
moizmb <mmb@pykih.com>
2017-08-18 15:09:59 +0530

    Updates api to give image for a link embedded in a tweet

--- app/controllers/api/v1/utilities_controller.rb
+++ app/controllers/api/v1/utilities_controller.rb
@@ -21,11 +21,42 @@ class Api::V1::UtilitiesController < ApiController
           flat_hash[meta_attr] = val
         end
 
+        # This block to get the image from a tweet if present
         if response["links"].present? && response["links"]["thumbnail"].present?
           flat_hash["thumbnail_url"] = response["links"]["thumbnail"][0]["href"]
           if response["links"]["thumbnail"][0]["media"].present?
             flat_hash["thumbnail_height"] = response["links"]["thumbnail"][0]["media"]["height"]
             flat_hash["thumbnail_width"] = response["links"]["thumbnail"][0]["media"]["width"]
+          end
+
+        # This block to get picture from an external link in tweet
+        elsif response["links"].present? && response["links"]["app"].present?
+          # Captures link of form https://t.co/link_id_as_hex
+          twitter_link_regex = /(https:\/\/)(t.co)\/([a-zA-Z\d]+)/
+          unless twitter_link_regex.match(response["links"]["app"][0]["html"]).nil?
+            link = twitter_link_regex.match(response["links"]["app"][0]["html"])[0]
+            begin
+              response = RestClient::Request.execute(
+                method: "get",
+                url: url,
+                headers: {
+                  params: {
+                    url: link
+                  }
+                }
+              )
+
+              response = JSON.parse(response)
+              if response["links"].present? && response["links"]["thumbnail"].present?
+                flat_hash["thumbnail_url"] = response["links"]["thumbnail"][0]["href"]
+                if response["links"]["thumbnail"][0]["media"].present?
+                  flat_hash["thumbnail_height"] = response["links"]["thumbnail"][0]["media"]["height"]
+                  flat_hash["thumbnail_width"] = response["links"]["thumbnail"][0]["media"]["width"]
+                end
+              end
+              rescue Exception => e
+                render json: {success: false, message: e.to_s}, status: 400
+            end
           end
         end
 
