957c97be7f52b6641f744199cddcddf1a4eef4ef
Moiz Mansur <moizmansur@gmail.com>
2017-08-23 13:02:33 +0530

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170818074601) do
+ActiveRecord::Schema.define(version: 20170818123336) do
 
   create_table "accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "username"
