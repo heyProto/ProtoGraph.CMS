85de99537fce46523e44c11b90c2a0ee46824168
Moiz Mansur <moizmansur@gmail.com>
2017-09-01 15:02:19 +0530

    Removes unneccessary comments

--- app/workers/csv_verification_worker.rb
+++ app/workers/csv_verification_worker.rb
@@ -9,7 +9,6 @@ class CsvVerificationWorker
     card_array_filtered = %x(csv2json #{@upload.attachment.file.file} | jq -f jq_filter.jq | jq -s '.')
     card_array_filtered = JSON.parse(card_array_filtered)
     schema = JSON.parse(RestClient.get(@upload.template_card.template_datum.schema_json))
-    # url = "#{AWS_API_DATACAST_URL}/pub/validate-json"
     card_array_filtered.each do |card_filtered|
       CardUploadWorker.perform_async(@upload.id, card_filtered, "name", "seo_blockquote_text", "source")
     end
