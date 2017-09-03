193541d3d628fbb189d26710b577d8a702f776a0
Amit Badheka <ab@pykih.com>
2017-09-03 15:44:24 +0530

    #112

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170903070333) do
+ActiveRecord::Schema.define(version: 20170903072406) do
 
   create_table "accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "username"
