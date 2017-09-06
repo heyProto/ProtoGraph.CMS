ea2ce4d5764c21c72ef176e571c9f7b88c685c58
Moiz Mansur <moizmansur@gmail.com>
2017-09-06 13:23:46 +0530

    Fixes conflit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826080800) do
+ActiveRecord::Schema.define(version: 20170904084802) do
 
   create_table "accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "username"
@@ -23,15 +23,46 @@ ActiveRecord::Schema.define(version: 20170826080800) do
     t.datetime "updated_at", null: false
     t.string "cdn_provider"
     t.string "cdn_id"
-    t.text "invalidation_endpoint"
+    t.text "host"
     t.text "cdn_endpoint"
-    t.string "authorization_header_name"
     t.string "client_token"
     t.string "access_token"
     t.string "client_secret"
+    t.integer "logo_image_id"
     t.index ["domain"], name: "index_accounts_on_domain"
     t.index ["slug"], name: "index_accounts_on_slug", unique: true
     t.index ["username"], name: "index_accounts_on_username", unique: true
+  end
+
+  create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id"
+    t.string "action"
+    t.integer "trackable_id"
+    t.string "trackable_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "folder_id"
+    t.integer "account_id"
+  end
+
+  create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "account_id"
+    t.integer "folder_id"
+    t.integer "cover_image_id"
+    t.string "title"
+    t.text "summary"
+    t.text "content"
+    t.string "genre"
+    t.text "og_image_variation_id"
+    t.integer "og_image_width"
+    t.integer "og_image_height"
+    t.text "twitter_image_variation_id"
+    t.integer "created_by"
+    t.integer "updated_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "url"
+    t.string "slug"
   end
 
   create_table "authentications", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -75,7 +106,6 @@ ActiveRecord::Schema.define(version: 20170826080800) do
 
   create_table "image_variations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "image_id"
-    t.text "image_url"
     t.text "image_key"
     t.integer "image_width"
     t.integer "image_height"
@@ -106,6 +136,7 @@ ActiveRecord::Schema.define(version: 20170826080800) do
     t.integer "updated_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_logo", default: false
   end
 
   create_table "permission_invites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -149,6 +180,38 @@ ActiveRecord::Schema.define(version: 20170826080800) do
     t.datetime "updated_at", null: false
     t.integer "sort_order"
     t.index ["slug"], name: "index_ref_roles_on_slug", unique: true
+  end
+
+  create_table "stream_entities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "stream_id"
+    t.string "entity_type"
+    t.string "entity_value"
+    t.boolean "is_excluded"
+    t.integer "created_by"
+    t.integer "updated_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "streams", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "title"
+    t.string "slug"
+    t.text "description"
+    t.integer "folder_id"
+    t.integer "account_id"
+    t.string "datacast_identifier"
+    t.integer "created_by"
+    t.integer "updated_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "card_count"
+    t.datetime "last_published_at"
+    t.string "order_by_key"
+    t.string "order_by_value"
+    t.integer "limit"
+    t.integer "offset"
+    t.index ["description"], name: "index_streams_on_description", type: :fulltext
+    t.index ["title"], name: "index_streams_on_title", type: :fulltext
   end
 
   create_table "taggings", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -202,6 +265,7 @@ ActiveRecord::Schema.define(version: 20170826080800) do
     t.boolean "has_static_image", default: false
     t.string "git_repo_name"
     t.string "s3_identifier"
+    t.boolean "has_multiple_uploads", default: false
     t.index ["slug"], name: "index_template_cards_on_slug", unique: true
   end
 
@@ -217,6 +281,22 @@ ActiveRecord::Schema.define(version: 20170826080800) do
     t.string "status"
     t.string "s3_identifier"
     t.index ["slug"], name: "index_template_data_on_slug", unique: true
+  end
+
+  create_table "uploads", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "attachment"
+    t.bigint "template_card_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "account_id"
+    t.bigint "folder_id"
+    t.integer "created_by"
+    t.integer "updated_by"
+    t.text "upload_errors"
+    t.text "filtering_errors"
+    t.index ["account_id"], name: "index_uploads_on_account_id"
+    t.index ["folder_id"], name: "index_uploads_on_folder_id"
+    t.index ["template_card_id"], name: "index_uploads_on_template_card_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -256,9 +336,14 @@ ActiveRecord::Schema.define(version: 20170826080800) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "seo_blockquote"
-    t.text "render_screenshot_url"
+    t.text "render_screenshot_key"
     t.text "status"
     t.integer "folder_id"
+    t.boolean "is_invalidating"
+    t.index ["slug"], name: "index_view_casts_on_slug", unique: true
   end
 
+  add_foreign_key "uploads", "accounts"
+  add_foreign_key "uploads", "folders"
+  add_foreign_key "uploads", "template_cards"
 end
