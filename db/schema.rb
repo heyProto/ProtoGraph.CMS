b5cd40481710473587e285077382f9526e9fc1f2
AashutoshSBhatt <aashutosh.bhatt@pykih.com>
2017-09-11 18:54:34 +0530

    Fixes layout for images index page.

--- db/schema.rb
+++ db/schema.rb
@@ -258,6 +258,7 @@ ActiveRecord::Schema.define(version: 20170906065137) do
     t.boolean "has_static_image", default: false
     t.string "git_repo_name"
     t.string "s3_identifier"
+    t.boolean "has_multiple_uploads", default: false
     t.index ["slug"], name: "index_template_cards_on_slug", unique: true
   end
 
@@ -273,6 +274,22 @@ ActiveRecord::Schema.define(version: 20170906065137) do
     t.string "s3_identifier"
     t.string "status"
     t.index ["slug"], name: "index_template_data_on_slug", unique: true
+  end
+
+  create_table "uploads", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "attachment"
+    t.bigint "template_card_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "account_id"
+    t.bigint "folder_id"
+    t.integer "created_by"
+    t.integer "updated_by"
+    t.text "upload_errors"
+    t.text "filtering_errors"
+    t.index ["account_id"], name: "index_uploads_on_account_id"
+    t.index ["folder_id"], name: "index_uploads_on_folder_id"
+    t.index ["template_card_id"], name: "index_uploads_on_template_card_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -316,6 +333,10 @@ ActiveRecord::Schema.define(version: 20170906065137) do
     t.text "status"
     t.integer "folder_id"
     t.boolean "is_invalidating"
+    t.index ["slug"], name: "index_view_casts_on_slug", unique: true
   end
 
+  add_foreign_key "uploads", "accounts"
+  add_foreign_key "uploads", "folders"
+  add_foreign_key "uploads", "template_cards"
 end
