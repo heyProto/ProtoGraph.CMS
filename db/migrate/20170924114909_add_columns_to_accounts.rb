5e683ce4bf91673a3181e20b873c74ac90c4fd5f
Moiz Mansur <moizmansur@gmail.com>
2017-09-25 16:35:08 +0530

    Adds default color

--- db/migrate/20170924114909_add_columns_to_accounts.rb
+++ db/migrate/20170924114909_add_columns_to_accounts.rb
@@ -1,5 +1,5 @@
 class AddColumnsToAccounts < ActiveRecord::Migration[5.1]
   def change
-    add_column :accounts, :house_colour, :string
+    add_column :accounts, :house_colour, :string, default: "#55a8fd"
   end
 end
