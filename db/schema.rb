bf40f4821fb5dc49130a166bcc2507dccc3bd636
Ritvvij Parrikh <ritvvij.parrikh@pykih.com>
2017-10-04 00:56:44 +0530

    q

--- db/schema.rb
+++ db/schema.rb
@@ -99,7 +99,6 @@ ActiveRecord::Schema.define(version: 20170929160624) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_trash", default: false
-    t.boolean "is_archived", default: false
   end
 
   create_table "friendly_id_slugs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
