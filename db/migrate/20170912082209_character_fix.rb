1587b77a39b40d8c67b8cea36fbc9f4fbfe8e5b0
Amit Badheka <ab@pykih.com>
2017-10-16 13:23:23 +0530

    Converting character encoding of images table to UTF-8

--- db/migrate/20170912082209_character_fix.rb
+++ db/migrate/20170912082209_character_fix.rb
@@ -1,5 +1,6 @@
 class CharacterFix < ActiveRecord::Migration[5.1]
   def change
     execute('ALTER TABLE articles CONVERT TO CHARACTER SET utf8;')
+    execute('ALTER TABLE images CONVERT TO CHARACTER SET utf8;')
   end
 end
