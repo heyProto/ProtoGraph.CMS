ae4e35442fe57d2b49a99e427d23a048dbd1b594
Moiz Mansur Bookwala <moizmansur@gmail.com>
2017-12-26 22:05:27 +0530

    Adds CRUD and publish method for ref_link_sources

---
+++ app/models/ref_link_source.rb
@@ -0,0 +1,48 @@
+# == Schema Information
+#
+# Table name: ref_link_sources
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  url         :text(65535)
+#  favicon_url :text(65535)
+#  created_by  :integer
+#  updated_by  :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class RefLinkSource < ApplicationRecord
+  #CONSTANTS
+  #CUSTOM TABLES
+  #GEMS
+  #ASSOCIATIONS
+  belongs_to :creator, class_name: "User", foreign_key: "created_by"
+  belongs_to :updator, class_name: "User", foreign_key: "updated_by"
+  #ACCESSORS
+  #VALIDATIONS
+  validates :name, presence: true
+  validates :favicon_url, presence: true
+  validates :url, presence: true
+  #CALLBACKS
+  #SCOPE
+  #OTHER
+  def as_json
+    {
+      name: name,
+      url: url,
+      favicon_url: favicon_url
+    }
+  end
+
+  def self.publish
+    ref_link_sources = RefLinkSource.all.as_json
+    response = Api::ProtoGraph::Utility.upload_to_cdn(Base64.encode64(ref_link_sources.to_json), "Assets/ref_link_sources.json", "application/json")
+    if response[0]["s3_endpoint"].present?
+      return true
+    else
+      return false
+    end
+  end
+  #PRIVATE
+end
