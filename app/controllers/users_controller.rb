91212322392b9841259ce369422f41172ce2aa21
Ritvvij Parrikh <ritvvij.parrikh@pykih.com>
2018-01-17 20:00:18 +0530

    settings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,21 @@ class UsersController < ApplicationController
         @accounts = current_user.accounts
         @account = Account.new
     end
+    
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+          redirect_to edit_user_registration_path, notice: t('cs')
+      else
+          render edit_user_registration_path, alert: @user.errors.full_messages
+      end
+      
+    end
+    
+    private
+
+      def user_params
+        params.require(:user).permit(:name, :email)
+      end
+    
 end
