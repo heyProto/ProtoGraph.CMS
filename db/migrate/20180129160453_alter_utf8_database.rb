ac5d5c17b10fd1cb51b787253ed1fb4105944e28
Amit Badheka <ab@pykih.com>
2018-01-29 21:42:11 +0530

    Converting tables to utf-8

--- db/migrate/20180129160453_alter_utf8_database.rb
+++ db/migrate/20180129160453_alter_utf8_database.rb
@@ -4,9 +4,9 @@ class AlterUtf8Database < ActiveRecord::Migration[5.1]
   end
 
   def up
-    execute "ALTER DATABASE `#{db.current_database}` CHARACTER SET utf8mb4;"
+    execute "ALTER DATABASE `#{db.current_database}` CHARACTER SET utf8;"
     db.tables.each do |table|
-      execute "ALTER TABLE `#{table}` CHARACTER SET = utf8mb4;"
+      execute "ALTER TABLE `#{table}` CHARACTER SET = utf8;"
     end
   end
 end
