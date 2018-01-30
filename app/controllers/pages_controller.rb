1dcbb6aaf344c2b26c50723618df32ca810ba0a6
Ritvvij Parrikh <ritvvij.parrikh@pykih.com>
2018-01-30 13:50:37 +0530

    friendly ?

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -75,7 +75,7 @@ class PagesController < ApplicationController
   private
 
     def set_page
-      @page = Page.find(params[:id])
+      @page = Page.friendly.find(params[:id])
     end
 
     def page_params
