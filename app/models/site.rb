26b5ff1168e8b4149c614025dc37196b063db92a
Amit Badheka <ab@pykih.com>
2018-01-31 20:53:59 +0530

    fixing Flow

--- app/models/site.rb
+++ app/models/site.rb
@@ -157,6 +157,7 @@ class Site < ApplicationRecord
         self.primary_language = "English" if self.primary_language.nil?
         self.english_name = self.name
         self.header_background_color = '#FFFFFF'
+        self.header_positioning = "left"
         true
     end
 
@@ -166,7 +167,6 @@ class Site < ApplicationRecord
         self.client_secret = ENV['AWS_SECRET_ACCESS_KEY'] if self.client_secret.blank?
         self.host = "#{AWS_API_DATACAST_URL}/cloudfront/invalidate" if self.host.blank?
         self.cdn_id = ENV['AWS_CDN_ID'] if self.cdn_id.blank? and self.cdn_endpoint == ENV['AWS_S3_ENDPOINT']
-        self.header_positioning = "left"
         self.english_name = self.name if self.is_english
         true
     end
