addc0a0d6deec37ea810aca7b0b66d59112c385f
Amit Badheka <ab@pykih.com>
2018-01-31 19:52:02 +0530

    Path typo fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -85,7 +85,7 @@ class PagesController < ApplicationController
           if @page.folder.present?
             redirect_to account_site_pages_path(@account, @site, folder_id: @page.folder.id), notice: 'Page was successfully updated.'
           else
-            redirect_to manager_account_site_path(@account, @site), notice: 'Page was successfully updated.'
+            redirect_to manager_account_site_pages_path(@account, @site), notice: 'Page was successfully updated.'
           end
         }
       else
