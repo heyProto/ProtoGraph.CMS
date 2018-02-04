a5e8960b9817ae2aae964970c2269ccb0d9ccb1f
Ritvvij Parrikh <ritvvij.parrikh@pykih.com>
2018-02-04 15:18:35 +0530

    rename to ProtoType

--- app/controllers/api/v1/template_cards_controller.rb
+++ app/controllers/api/v1/template_cards_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::TemplateCardsController < ApiController
 
     def index
         if @folder.vertical.present?
-          @template_cards = @account.template_cards.where(is_current_version: true).where(name: ['toParagraph', 'toImage', 'toVideo: Youtube', 'toCluster', 'toQuiz', 'toTimeline', 'toStory'])
+          @template_cards = @account.template_cards.where(is_current_version: true, name: ['toParagraph', 'toImage', 'toVideo: Youtube', 'toCluster', 'toQuiz', 'toTimeline', 'toStory'])
         else
-          @template_cards = @account.template_cards.where(is_current_version: true).where(name: ['toQuiz', 'toTimeline'])
+          @template_cards = @account.template_cards.where(is_current_version: true, name: ['toQuiz', 'toTimeline'])
         end
         render json: {template_cards: @template_cards.as_json(only: [:account_id, :id, :slug, :global_slug,:name, :template_datum_id, :git_repo_name], methods: [:account_slug, :icon_url])}
     end
