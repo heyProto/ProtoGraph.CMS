5106ae6f4ad0291991dde3b5371326fe8c62898e
Ritvvij Parrikh <ritvvij.parrikh@pykih.com>
2018-02-18 17:39:15 +0530

    s

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -209,7 +209,7 @@ class PagesController < ApplicationController
 
     def page_params
       params.require(:page).permit(:id, :account_id, :site_id, :folder_id, :headline, :meta_keywords, :meta_description, :summary, :template_page_id, :byline, :one_line_concept,
-                                   :cover_image_url, :cover_image_url_7_column, :cover_image_url_facebook, :cover_image_url_square, :cover_image_alignment, 
+                                   :cover_image_url, :cover_image_url_7_column, :cover_image_url_facebook, :cover_image_url_square, :cover_image_alignment, :content,
                                    :is_sponsored, :is_interactive, :has_data, :has_image_other_than_cover, :has_audio, :has_video, :status, :published_at, :url, 
                                    :ref_category_series_id, :ref_category_intersection_id, :ref_category_sub_intersection_id, :view_cast_id, :page_object_url, :created_by, 
                                    :updated_by, :english_headline, :due, :description, :cover_image_id_4_column, :cover_image_id_3_column, :cover_image_id_2_column, :cover_image_credit, :share_text_facebook, 
