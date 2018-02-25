cb24c82fe805002ccc796f10d3fc02e084c8c96a
Ritvvij Parrikh <ritvvij.parrikh@pykih.com>
2018-02-25 21:55:21 +0530

    propogatable

--- app/models/concerns/propagatable.rb
+++ app/models/concerns/propagatable.rb
@@ -8,22 +8,35 @@ module Propagatable
     private
     
     def propogate_updated_at
-      if self.class.to_s == "UserEmail" or (self.class.to_s == "Authentication" and self.user_id.present?)
-        if self.saved_changes.present?
-          self.user.updated_at = Time.now
+      if self.saved_changes.present?
+        if ["ViewCast", "Stream"].index(self.class.to_s).present?
+          self.folder.update_attributes(updated_at: Time.now)               if self.folder.present?
+        elsif ["Page"].index(self.class.to_s).present?
+          self.folder.update_attributes(updated_at: Time.now)               if self.folder.present?
+          self.series.update_attributes(updated_at: Time.now)               if self.series.present?
+          self.intersection.update_attributes(updated_at: Time.now)         if self.intersection.present?
+          self.sub_intersection.update_attributes(updated_at: Time.now)     if self.sub_intersection.present?
+        elsif self.class.to_s == "SiteVerticalNavigation"
+          self.ref_category.update_attributes(updated_at: Time.now)         if self.ref_category.present?
+        elsif ["Image", "Audio", "RefCategory", "Folder"].index(self.class.to_s).present?
+          self.site.update_attributes(updated_at: Time.now)                 if self.site.present?
+        elsif ["PageStream", "PageTodo"].index(self.class.to_s).present?
+          self.page.update_attributes(updated_at: Time.now)                 if self.page.present?
+        elsif ["Site"].index(self.class.to_s).present?
+          self.account.update_attributes(updated_at: Time.now)              if self.account.present?
+        elsif ["UserEmail"].index(self.class.to_s).present?
+          self.user.update_attributes(updated_at: Time.now)                 if self.user.present?
+        elsif ["ImageVariation"].index(self.class.to_s).present?
+          self.image.update_attributes(updated_at: Time.now)                if self.image.present?
+        elsif ["AudioVariation"].index(self.class.to_s).present?
+          self.audio.update_attributes(updated_at: Time.now)                if self.audio.present?
+        elsif self.class.to_s == "Authentication" and self.user_id.present?
+          self.user.update_attributes(updated_at: Time.now)                 if self.user.present?
+        elsif self.class.to_s == "Authentication" and self.site_id.present?
+          self.site.update_attributes(updated_at: Time.now)                 if self.site.present?
         end
       end
       true
-      
-      # ["Image", "Audio", "RefCategory", "Folder", "Authentication"] --> SITE
-      # ["Site"] --> ACCOUNT
-      # ["UserEmail", "Authentication"] --> USER
-      # ["PageStream", "PageTodo", "SiteVerticalNavigation"] --> PAGE
-      # ["ImageVariation"] --> IMAGE
-      # ["AudioVariation"] --> AUDIO
-      # ["Page"] --> REF_CATEGORY
-      # ["ViewCast", "Page", "Stream"] --> FOLDER
-      
       
       #TODO AMIT - How do we handle this for Permission and PermissionInvite
       #-----------
