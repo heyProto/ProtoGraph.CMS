517d6fe931d341dc2234662c6ce664b3b42df904
Amit Badheka <ab@pykih.com>
2018-03-28 15:06:05 +0530

    Sorting in byline stream

--- app/models/site.rb
+++ app/models/site.rb
@@ -230,7 +230,7 @@ class Site < ApplicationRecord
         self.permissions.not_hidden.each do |p|
             members << [p.name, p.id]
         end
-        members
+        members.sort_by{|b| b[0]}
     end
 
     private
