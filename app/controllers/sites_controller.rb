e427c549ac090f76b4b94e8b9dabc923c713e354
Ritvvij Parrikh <ritvvij.parrikh@pykih.com>
2018-04-15 13:26:07 +0530

    workspace

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -3,13 +3,6 @@ class SitesController < ApplicationController
   before_action :authenticate_user!
   before_action :sudo_role_can_add_site, only: [:new, :create]
   before_action :sudo_role_can_site_settings, only: [:edit, :update]
-
-  def show
-    @folder = Folder.new
-    @folders = @permission_role.can_see_all_folders ? @site.folders.active : current_user.folders(@site).active
-    @activities = [] #@account.activities.order("updated_at DESC").limit(30) Need to update the logic for permission
-    render layout: "z"
-  end
   
   def remove_favicon
     @site.update_attributes(favicon_id: nil)
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,11 @@ class StaticPagesController < ApplicationController
       if current_user.accounts.count  == 0
         redirect_to new_account_path
       else current_user.accounts.count > 1
-        redirect_to account_site_path(current_user.accounts.first, current_user.accounts.first.site)
+        account = current_user.accounts.first
+        site = current_user.accounts.first.site
+        permission = current_user.owner_role(account.id) || current_user.permission_object(site.id)
+        folder = permission.permission_role.can_see_all_folders ? site.folders.active.where(is_trash: false).first : current_user.folders(site).active.where(is_trash: false).first
+        redirect_to account_site_folder_view_casts_path(account, site, folder)
       end
     else
       redirect_to new_user_session_path
