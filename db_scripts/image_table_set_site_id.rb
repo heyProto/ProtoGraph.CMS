3fdd552f85d4f4828f960393eda9aee0b3c5627e
deepbodra96 <deepbodra96@gmail.com>
2018-06-23 16:26:50 +0530

    task:account2site

--- db_scripts/image_table_set_site_id.rb
+++
@@ -1,9 +0,0 @@
-Image.find_each do |image|
-    account_id = image.account_id
-    if Account.exists?(id: account_id)
-        account=Account.find(account_id)
-        image.update_columns(site_id: account.site.id)
-    else
-        puts "Account with id=#{account_id} not found"
-    end
-end
