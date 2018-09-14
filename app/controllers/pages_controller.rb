b0a0bf3a1044cd13234b2329715bc8a4efb70aa8
Amit Badheka <ab@pykih.com>
2018-09-14 14:38:56 +0530

    fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -143,7 +143,7 @@ class PagesController < ApplicationController
       params.require(:page).permit(:id, :site_id, :folder_id, :folder_id, :headline, :meta_keywords, :meta_description, :summary, :template_page_id, :byline_id, :one_line_concept, :hide_byline,
                                     :reported_from_country, :reported_from_state, :reported_from_district, :reported_from_city,
                                    :cover_image_url, :cover_image_url_7_column, :cover_image_url_facebook, :cover_image_url_square, :cover_image_alignment, :content,
-                                   :is_sponsored, :is_interactive, :has_data, :has_image_other_than_cover, :has_audio, :has_video, :status, :published_at, :url,
+                                   :is_sponsored, :is_interactive, :has_data, :has_image_other_than_cover, :has_audio, :has_video, :status, :published_at, :url, :html_key,
                                    :ref_category_series_id, :ref_category_intersection_id, :ref_category_sub_intersection_id, :view_cast_id, :page_object_url, :created_by,
                                    :updated_by, :english_headline, :due, :description, :cover_image_id_4_column, :cover_image_id_3_column, :cover_image_id_2_column, :cover_image_credit, :share_text_facebook,
                                      :share_text_twitter, :publish, :prepare_cards_for_assembling, :format, :importance, :external_identifier, collaborator_lists: [], cover_image_attributes: [:image, :site_id, :is_cover, :created_by,
